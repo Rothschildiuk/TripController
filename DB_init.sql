dc887a3a8a84eaaa6af8b6df08cf5f0c3ad982f0
Oleh Baidiuk <oleh12@gmail.com>
2019-03-16 12:46:52 +0100

    add DB_init and table contacts

---
+++ DB_init.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `contacts`
+(
+  `id`       INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `forename` VARCHAR(255)     NOT NULL,
+  `surname`  VARCHAR(255)     NOT NULL,
+  `phone`    VARCHAR(255)     NOT NULL,
+  `address`   VARCHAR(255)     NOT NULL,
+  `comment`  VARCHAR(255)     NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+
