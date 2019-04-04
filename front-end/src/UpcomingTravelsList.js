b0b80cfe70fb3f34051b4d9df72ecb039d7478d0
Oleh Baidiuk <oleh12@gmail.com>
2019-04-04 19:10:44 +0200

    bugFixing

--- front-end/src/UpcomingTravelsList.js
+++ front-end/src/UpcomingTravelsList.js
@@ -1,44 +1,34 @@
-import React, {useEffect, useState} from 'react'
+import React from 'react'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
-import axios from 'axios'
 import 'typeface-roboto'
 import Typography from '@material-ui/core/Typography'
 import AddNewDate from './AddNewDate'
 
 
-const UpcomingTravelsList = () => {
-
-    const [upcomingTravels, setUpcomingTravels] = useState([0])
-    const url = '/api/getUpcomingTravels'
-
-    useEffect(() => {
-            axios.get(url)
-                .then(resp => setUpcomingTravels(resp.data))
-                .catch(err => console.log(err))
-
-        }, [] //todo add upcomingTravels (updateble)
-    )
+const UpcomingTravelsList = (props) => {
 
 
-    return (
-        <div className='UpcomingTravelsList'>
-            <Typography variant='h4' align='center'>
-                Actual travels
-            </Typography>
-            <List>
-                {
-                    upcomingTravels.map((item, index) =>
-                        <ListItem key={index} button >
-                            <ListItemText primary={item.date}/>
-                        </ListItem>)
-                }
-            </List>
-            <AddNewDate/>
+    return <div className='UpcomingTravelsList'>
 
-        </div>
-    )
+        <Typography variant='h4' align='center'>
+            Actual travels
+
+
+        </Typography>
+
+        <List>
+            {props.travels.map((item, index) =>
+                <ListItem key={index} button>
+                <ListItemText primary={item.date}/>
+            </ListItem>)
+            }
+        </List>
+        <AddNewDate/>
+
+    </div>
+
 }
 
 export default UpcomingTravelsList
--- front-end/src/WorkingArea.js
+++ front-end/src/WorkingArea.js
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './App.css'
 import UpcomingTravelsList from './UpcomingTravelsList'
 import AddPassenger from './AddPassenger'
+import axios from 'axios'
 
 const WorkingArea = () => {
 
-    return (
+    const [upcomingTravels, setUpcomingTravels] = useState([])
+    const url = '/api/getUpcomingTravels'
 
-            <div className='WorkingArea'>
-                <UpcomingTravelsList/>
-                <AddPassenger/>
-            </div>
-    );
 
-};
+    useEffect(() => {
+            axios.get(url)
+                .then(resp => setUpcomingTravels(resp.data))
+        }, [] //todo add upcomingTravels (updateble)
+    )
+    return <div className='WorkingArea'>
+        {upcomingTravels.length !== 0 && <UpcomingTravelsList travels={upcomingTravels}/>}
+
+
+        <AddPassenger/>
+    </div>
+
+
+}
 
 export default WorkingArea
