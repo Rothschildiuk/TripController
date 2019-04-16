ae102259dcabea25c26dd1bf54b9118903f17221
Oleh Baidiuk <oleh12@gmail.com>
2019-04-17 00:01:01 +0200

    add Login Dialog

--- front-end/src/Header.js
+++ front-end/src/Header.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {AppBar, Toolbar, Typography, Button} from '@material-ui/core'
+import Dialog from '@material-ui/core/Dialog'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import DialogActions from '@material-ui/core/DialogActions'
 
 const Header = () => {
+
+    const [open, setOpen] = useState(false)
+
+    function openLogin() {
+        setOpen(!open)
+    }
 
     return <div className='Header'>
         <AppBar position='static'>
@@ -9,7 +18,20 @@ const Header = () => {
                 <Typography variant='h6' color='inherit'>
                     TripController
                 </Typography>
-                <Button color='inherit'>Login</Button>
+                <Button color='inherit' onClick={() => openLogin()}>Login</Button>
+
+
+                <Dialog open={open}>
+                    <DialogTitle id="customized-dialog-title" onClose={openLogin}>
+                        LoggIn
+                    </DialogTitle>
+
+                    <DialogActions>
+                        <Button onClick={openLogin} color="primary">
+                            Close
+                        </Button>
+                    </DialogActions>
+                </Dialog>
             </Toolbar>
         </AppBar>
     </div>
