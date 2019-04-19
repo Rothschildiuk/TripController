f4283e43eec6340c1bc2f74dc79a4754839447e5
Oleh Baidiuk <oleh12@gmail.com>
2019-04-19 16:33:53 +0200

    add Login Button

--- front-end/src/Header.js
+++ front-end/src/Header.js
@@ -3,10 +3,20 @@ import {AppBar, Toolbar, Typography, Button} from '@material-ui/core'
 import Dialog from '@material-ui/core/Dialog'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import DialogActions from '@material-ui/core/DialogActions'
+import GoogleLogin from 'react-google-login'
 
 const Header = () => {
 
     const [open, setOpen] = useState(false)
+
+
+    const responseGoogle = (response) => {
+        console.log(response);
+        console.log(response.profileObj.googleId)
+        console.log(response.profileObj.name)
+        console.log(response.profileObj.email)
+        console.log(response.profileObj.imageUrl)
+    }
 
     function openLogin() {
         setOpen(!open)
@@ -19,7 +29,13 @@ const Header = () => {
                     TripController
                 </Typography>
                 <Button color='inherit' onClick={() => openLogin()}>Login</Button>
-
+                <GoogleLogin
+                    clientId="499697510229-gra6n5ntbqo8ngp372dtu5dg68qjuc6k.apps.googleusercontent.com"
+                    buttonText="Login"
+                    onSuccess={responseGoogle}
+                    onFailure={responseGoogle}
+                    cookiePolicy={'single_host_origin'}
+                />
 
                 <Dialog open={open}>
                     <DialogTitle id="customized-dialog-title" onClose={openLogin}>
